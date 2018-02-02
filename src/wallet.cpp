39ae002d9f7fa34369bd526eb7db9ef5ac2d10a8
lateminer <late-miner@ya.ru>
2018-02-02 20:25:42 +0300

    Do not exclude stake from balances in listaddressgroupings RPC

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2093,7 +2093,7 @@ std::map<CTxDestination, int64_t> CWallet::GetAddressBalances()
             if (!IsFinalTx(*pcoin) || !pcoin->IsTrusted())
                 continue;
 
-            if ((pcoin->IsCoinBase() || pcoin->IsCoinStake()) && pcoin->GetBlocksToMaturity() > 0)
+            if (pcoin->IsCoinBase() && pcoin->GetBlocksToMaturity() > 0)
                 continue;
 
             int nDepth = pcoin->GetDepthInMainChain();
